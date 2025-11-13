45d5e5e50f644ecd6733f415691b19c375953322
Zainab Shittu <Zainab@Morio.local>
2025-11-12 21:05:05 -0600

    removed temporary /__db__/ endpoint

--- core_config/core/urls.py
+++ core_config/core/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static 
-from posts.views import db_health
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')), 
     path('', include('posts.urls')), 
-    path('__db__/', db_health),     
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
